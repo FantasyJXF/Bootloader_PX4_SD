6141497ece515c062a2a2b5393b9a91675f53e73
liangzelang <liangzelang@gmail.com>
2016-07-15 10:56:22 +0800

    first, SB

---
+++ libopencm3/lib/sam/3n/pmc.d
@@ -0,0 +1,12 @@
+pmc.o: ../common/pmc.c ../../../include/libopencm3/sam/pmc.h \
+ ../../../include/libopencm3/cm3/common.h \
+ /usr/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h \
+ /usr/arm-none-eabi/include/stdint.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/arm-none-eabi/include/sys/features.h \
+ /usr/arm-none-eabi/include/sys/_intsup.h \
+ /usr/arm-none-eabi/include/sys/_stdint.h \
+ /usr/lib/gcc/arm-none-eabi/4.9.3/include/stdbool.h \
+ ../../../include/libopencm3/sam/memorymap.h \
+ ../../../include/libopencm3/sam/3n/memorymap.h \
+ ../../../include/libopencm3/sam/eefc.h
---
+++ libopencm3/lib/sam/3x/gpio.d
@@ -0,0 +1,12 @@
+gpio.o: ../common/gpio.c ../../../include/libopencm3/sam/gpio.h \
+ ../../../include/libopencm3/sam/pio.h \
+ ../../../include/libopencm3/cm3/common.h \
+ /usr/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h \
+ /usr/arm-none-eabi/include/stdint.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/arm-none-eabi/include/sys/features.h \
+ /usr/arm-none-eabi/include/sys/_intsup.h \
+ /usr/arm-none-eabi/include/sys/_stdint.h \
+ /usr/lib/gcc/arm-none-eabi/4.9.3/include/stdbool.h \
+ ../../../include/libopencm3/sam/memorymap.h \
+ ../../../include/libopencm3/sam/3x/memorymap.h
